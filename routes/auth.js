642ded9531b705ff3a78d341a84e9863e68895fd
klogpok <kloleg@gmail.com>
2018-06-01 17:25:01 +0300

    Auth callback

--- routes/auth.js
+++ routes/auth.js
@@ -7,4 +7,12 @@ router.get(
   passport.authenticate('google', { scope: ['profile', 'email'] })
 );
 
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/' }),
+  (req, res) => {
+    res.redirect('/dashboard');
+  }
+);
+
 module.exports = router;
