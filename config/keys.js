01100756138cd311926453881d502ebc18f7d620
klogpok <kloleg@gmail.com>
2018-06-12 23:02:02 +0300

    production key values

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-  googleClientID:
-    '578893624065-ont8vd0ec0uq9rht89rsviheomupqrbe.apps.googleusercontent.com',
-  googleClientSecret: 'gn81gMo6ex1YmynNC6DUUkYn',
-  mongoURL: 'mongodb://Oleg:000000oleg@ds237610.mlab.com:37610/storybooks-dev'
-};
+if (process.env.NODE_ENV == 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
